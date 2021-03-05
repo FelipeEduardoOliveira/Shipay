e0380c7914dca8d3f2a6a7a1387f77a6f080469c
Felipe Oliveira <felipe.oliveira@pagtel.com.br>
2021-03-05 10:09:51 -0300

    Deploy final

--- src/components/newTransaction.js
+++ src/components/newTransaction.js
@@ -20,9 +20,16 @@ class NewTransaction extends Component {
     };
   }
   componentDidMount() {
-    // this.setState({
-    //   transactions: JSON.parse(localStorage.getItem("transaction")),
-    // });
+    const valores = JSON.parse(localStorage.getItem("transaction"));
+    if (valores !== null) {
+      this.setState({
+        transactions: valores,
+      });
+    } else {
+      this.setState({
+        transactions: [],
+      });
+    }
   }
   handleSubmit(e) {
     console.log("chegou");
@@ -48,103 +55,103 @@ class NewTransaction extends Component {
       this.setState({ errorMensage: "Digite o valor da compra" });
       return;
     }
-    // this.isValidCPF(state.client, state.establishment);
-    this.saveTransaction();
+    this.isValidCPF(state.client, state.establishment);
+    // this.saveTransaction();
   }
 
-  // isValidCPF(cpf, cnpj) {
-  //   if (typeof cpf !== "string") return false;
-  //   cpf = cpf.replace(/[\s.-]*/gim, "");
-  //   if (cpf.length != 11)
-  //     return this.setState({ errorMensage: "CPF Incompleto" });
-  //   if (
-  //     !cpf ||
-  //     cpf.length != 11 ||
-  //     cpf == "00000000000" ||
-  //     cpf == "11111111111" ||
-  //     cpf == "22222222222" ||
-  //     cpf == "33333333333" ||
-  //     cpf == "44444444444" ||
-  //     cpf == "55555555555" ||
-  //     cpf == "66666666666" ||
-  //     cpf == "77777777777" ||
-  //     cpf == "88888888888" ||
-  //     cpf == "99999999999"
-  //   ) {
-  //     this.setState({ errorMensage: "Não caiu aqui" });
-  //   }
-  //   var soma = 0;
-  //   var resto;
-  //   for (var i = 1; i <= 9; i++)
-  //     soma = soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
-  //   resto = (soma * 10) % 11;
-  //   if (resto == 10 || resto == 11) resto = 0;
-  //   if (resto != parseInt(cpf.substring(9, 10)))
-  //     return this.setState({
-  //       errorMensage: "CPF inválido",
-  //     });
-  //   soma = 0;
-  //   for (var i = 1; i <= 10; i++)
-  //     soma = soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
-  //   resto = (soma * 10) % 11;
-  //   if (resto == 10 || resto == 11) resto = 0;
-  //   if (resto != parseInt(cpf.substring(10, 11)))
-  //     return this.setState({ errorMensage: "CPF inválido" });
-  //   this.validarCNPJ(cnpj);
-  // }
+  isValidCPF(cpf, cnpj) {
+    if (typeof cpf !== "string") return false;
+    cpf = cpf.replace(/[\s.-]*/gim, "");
+    if (cpf.length != 11)
+      return this.setState({ errorMensage: "CPF Incompleto" });
+    if (
+      !cpf ||
+      cpf.length != 11 ||
+      cpf == "00000000000" ||
+      cpf == "11111111111" ||
+      cpf == "22222222222" ||
+      cpf == "33333333333" ||
+      cpf == "44444444444" ||
+      cpf == "55555555555" ||
+      cpf == "66666666666" ||
+      cpf == "77777777777" ||
+      cpf == "88888888888" ||
+      cpf == "99999999999"
+    ) {
+      this.setState({ errorMensage: "Não caiu aqui" });
+    }
+    var soma = 0;
+    var resto;
+    for (var i = 1; i <= 9; i++)
+      soma = soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+    resto = (soma * 10) % 11;
+    if (resto == 10 || resto == 11) resto = 0;
+    if (resto != parseInt(cpf.substring(9, 10)))
+      return this.setState({
+        errorMensage: "CPF inválido",
+      });
+    soma = 0;
+    for (var i = 1; i <= 10; i++)
+      soma = soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    resto = (soma * 10) % 11;
+    if (resto == 10 || resto == 11) resto = 0;
+    if (resto != parseInt(cpf.substring(10, 11)))
+      return this.setState({ errorMensage: "CPF inválido" });
+    this.validarCNPJ(cnpj);
+  }
 
-  // validarCNPJ(cnpj) {
-  //   var cnpj = cnpj.replace(/[^\d]+/g, "");
+  validarCNPJ(cnpj) {
+    var cnpj = cnpj.replace(/[^\d]+/g, "");
 
-  //   if (cnpj == "") return false;
+    if (cnpj == "") return false;
 
-  //   if (cnpj.length != 14)
-  //     return this.setState({ errorMensage: "CNPJ Incompleto" });
+    if (cnpj.length != 14)
+      return this.setState({ errorMensage: "CNPJ Incompleto" });
 
-  //   // Elimina CNPJs invalidos conhecidos
-  //   if (
-  //     cnpj == "00000000000000" ||
-  //     cnpj == "11111111111111" ||
-  //     cnpj == "22222222222222" ||
-  //     cnpj == "33333333333333" ||
-  //     cnpj == "44444444444444" ||
-  //     cnpj == "55555555555555" ||
-  //     cnpj == "66666666666666" ||
-  //     cnpj == "77777777777777" ||
-  //     cnpj == "88888888888888" ||
-  //     cnpj == "99999999999999"
-  //   )
-  //     return false;
+    // Elimina CNPJs invalidos conhecidos
+    if (
+      cnpj == "00000000000000" ||
+      cnpj == "11111111111111" ||
+      cnpj == "22222222222222" ||
+      cnpj == "33333333333333" ||
+      cnpj == "44444444444444" ||
+      cnpj == "55555555555555" ||
+      cnpj == "66666666666666" ||
+      cnpj == "77777777777777" ||
+      cnpj == "88888888888888" ||
+      cnpj == "99999999999999"
+    )
+      return false;
 
-  //   // Valida DVs
-  //   var tamanho = cnpj.length - 2;
-  //   var numeros = cnpj.substring(0, tamanho);
-  //   var digitos = cnpj.substring(tamanho);
-  //   var soma = 0;
-  //   var pos = tamanho - 7;
-  //   for (var i = tamanho; i >= 1; i--) {
-  //     soma += numeros.charAt(tamanho - i) * pos--;
-  //     if (pos < 2) pos = 9;
-  //   }
-  //   var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
-  //   if (resultado != digitos.charAt(0))
-  //     return this.setState({ errorMensage: "CNPJ inválido" });
+    // Valida DVs
+    var tamanho = cnpj.length - 2;
+    var numeros = cnpj.substring(0, tamanho);
+    var digitos = cnpj.substring(tamanho);
+    var soma = 0;
+    var pos = tamanho - 7;
+    for (var i = tamanho; i >= 1; i--) {
+      soma += numeros.charAt(tamanho - i) * pos--;
+      if (pos < 2) pos = 9;
+    }
+    var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+    if (resultado != digitos.charAt(0))
+      return this.setState({ errorMensage: "CNPJ inválido" });
 
-  //   tamanho = tamanho + 1;
-  //   numeros = cnpj.substring(0, tamanho);
-  //   soma = 0;
-  //   pos = tamanho - 7;
-  //   for (var i = tamanho; i >= 1; i--) {
-  //     soma += numeros.charAt(tamanho - i) * pos--;
-  //     if (pos < 2) pos = 9;
-  //   }
-  //   resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
-  //   if (resultado != digitos.charAt(1))
-  //     return this.setState({ errorMensage: "CNPJ inválido" });
+    tamanho = tamanho + 1;
+    numeros = cnpj.substring(0, tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+    for (var i = tamanho; i >= 1; i--) {
+      soma += numeros.charAt(tamanho - i) * pos--;
+      if (pos < 2) pos = 9;
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+    if (resultado != digitos.charAt(1))
+      return this.setState({ errorMensage: "CNPJ inválido" });
 
-  //   // return true;
-  //   this.saveTransaction();
-  // }
+    // return true;
+    this.saveTransaction();
+  }
 
   async saveTransaction() {
     const newTransaction = {
@@ -175,7 +182,7 @@ class NewTransaction extends Component {
   }
 
   notify() {
-    toast.success("🦄 Wow so easy!", {
+    toast.success("Venda realizada com sucesso", {
       position: "top-center",
       autoClose: 2000,
       hideProgressBar: true,
@@ -258,9 +265,15 @@ class NewTransaction extends Component {
             <Button variant="contained" color="primary" type="submit">
               Finalizar venda
             </Button>
-            <Button variant="contained" color="secondary">
-              <Link to="/vendas">Vendas realizadas</Link>
-            </Button>
+            {this.state.transactions.length === 0 ? (
+              <Button variant="contained" color="secondary" disabled>
+                <Link to="/vendas">Não há vendas</Link>
+              </Button>
+            ) : (
+              <Button variant="contained" color="secondary">
+                <Link to="/vendas">Vendas realizadas</Link>
+              </Button>
+            )}
           </div>
         </form>
 
