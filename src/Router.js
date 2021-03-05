370a2c73a868fc04ff8adcd22b990be6dda18713
Felipe Oliveira <felipe.oliveira@pagtel.com.br>
2021-03-05 01:24:29 -0300

    Ajuste deploy

--- src/Router.js
+++ src/Router.js
@@ -6,7 +6,7 @@ import ViewTransaction from "./components/viewTransaction";
 
 function Router() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="shipay">
       <Switch>
         <Route exact path="/" component={NewTransaction} />
         <Route exact path="/vendas" component={ViewTransaction} />
